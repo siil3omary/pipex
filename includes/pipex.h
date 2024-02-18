eb298950ab98dfbb928de5398790644622c348b4
Ayman Elomari <aelomari@Aymans-MacBook-Pro.local>
2024-02-18 21:47:56 +0100

    add struct

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,32 +6,35 @@
 /*   By: aelomari <aelomari@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 13:55:57 by aelomari          #+#    #+#             */
-/*   Updated: 2024/02/18 21:41:36 by aelomari         ###   ########.fr       */
+/*   Updated: 2024/02/18 21:47:04 by aelomari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef PIPEX_H
-#define PIPEX_H
+# define PIPEX_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <strings.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <strings.h>
+# include <unistd.h>
 typedef struct s_pipex
 {
-	char **path;
-	char **pathCmd;
-    
-} t_pipex;
+	pid_t	pid1;
+	pid_t	pid2;
+	int		infileFd;
+	int		outfileFd;
+	char	**path;
+	char	**pathCmd;
 
-size_t ft_strlen(const char *s);
-void ft_putstr_fd(char *s, int fd);
-void ft_putchar_fd(char c, int fd);
-size_t ft_strlcpy(char *dest, const char *src, size_t size);
-char *ft_strjoin(char const *s1, char const *s2);
-char *ft_strdup(const char *s);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
-char	**ft_split(char const *s, char c);
+}			t_pipex;
+
+size_t		ft_strlen(const char *s);
+void		ft_putstr_fd(char *s, int fd);
+void		ft_putchar_fd(char c, int fd);
+size_t		ft_strlcpy(char *dest, const char *src, size_t size);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strdup(const char *s);
+char		*ft_strnstr(const char *big, const char *little, size_t len);
+char		**ft_split(char const *s, char c);
 #endif
