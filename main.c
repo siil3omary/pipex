f723569f271c597a8e588df6a43fa615925d5404
aelomari <L3omary@icloud.com>
2024-02-23 14:16:29 +0100

    nchofo linux ach kigol fhadchi

--- main.c
+++ main.c
@@ -1,46 +1,27 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-
-int main() {
+int main()
+{
     int status;
+    pid_t pid;
 
-    pid_t pid = fork();
-    if (pid < 0) {
+    pid = fork();
+    if(pid < 0)
+    {
         printf("error fork");
         exit(1);
     }
-    if (pid == 0) {
-        waitpid(0, &status, 0); // Wait for the child process to exit
-        fprintf(stdout, "Hello\n");
-
-
-
-
-    } else {
-        fprintf(stdout, "WORLD\n");
-
-        
-    }
+    waitpid(pid, &status, 0);
+    if(pid == 0)
+    {
+        printf("hello from %d", getpid());
         close(1);
-        system("leaks a.out");
-        exit(1);
+    }else{
+        printf("hello from %d", getpid());
 
-    // Execute leaks command only once after all processes have completed
 
-    return 0;
+    }
+    system("leaks a.out");
 }
