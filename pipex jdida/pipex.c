49a374a68c936329cf079b4b1437b0112ea67e4b
Ayman Elomari <aelomari@Aymans-MacBook-Pro.local>
2024-03-22 01:30:13 +0000

    push

--- pipex jdida/pipex.c
+++ pipex jdida/pipex.c
@@ -6,11 +6,23 @@
 /*   By: aelomari <aelomari@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 16:10:30 by aelomari          #+#    #+#             */
-/*   Updated: 2024/03/21 19:48:17 by aelomari         ###   ########.fr       */
+/*   Updated: 2024/03/22 01:28:16 by aelomari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+void	ft_putchar(char c)
+{
+	write(2, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	if (n > 9)
+		ft_putnbr(n / 10);
+	ft_putchar(n % 10 + '0');
+}
 
 int	main(int ac, char **av, char **env)
 {
@@ -35,9 +47,10 @@ int	main(int ac, char **av, char **env)
 	initstrct(pipex, ac, av, env);
 	// pipex excute
 	openfiles(pipex);
-	pipe(pipex->pipe_fd);
-	while (pipex->index < ac - 3)
+	pipex->index = 2;
+	while (pipex->index < ac - 1)
 	{
+		pipe(pipex->pipe_fd);
 		pipex->pid = fork();
 		if (pipex->pid == -1)
 		{
@@ -47,52 +60,57 @@ int	main(int ac, char **av, char **env)
 		}
 		if (pipex->pid == 0)
 		{
-			if (pipex->index == 0)
+			if (pipex->index == 2)
 			{
-				close(pipex->fd)
+				if (pipex->infile_fd == -1){
+					pipex->infile_fd = STDIN_FILENO;
+					
+				}
+				close(pipex->pipe_fd[0]);
 				dup2(pipex->infile_fd, 0);
 				dup2(pipex->pipe_fd[1], 1);
-				pipex->cmd = check_cmd(pipex->avs[pipex->index + 2], pipex);
+				close(pipex->pipe_fd[1]);
+				pipex->cmd = check_cmd(pipex->avs[pipex->index], pipex);
 				execve(pipex->cmd[0], pipex->cmd, pipex->envs);
 				free_all(pipex->cmd);
 				free(pipex);
 				exit(1);
 			}
-			else
+			else if (pipex->index != ac - 2)
 			{
-				if (pipex->index == pipex->acs - 4)
-				{
-					close(pipex->pipe_fd[1]);
-					dup2(pipex->outfile_fd, 1);
-					dup2(pipex->pipe_fd[0], 0);
-					close(pipex->pipe_fd[0]);
-					pipex->cmd = check_cmd(pipex->avs[pipex->index + 2], pipex);
-					execve(pipex->cmd[0], pipex->cmd, pipex->envs);
-					free(pipex);
-					free_all(pipex->cmd);
-					exit(1);
-				}
-				else
-				{
-
-					close(pipex->pipe_fd[0]);
-					dup2(pipex->pipe_fd[0], 0);
-					dup2(pipex->pipe_fd[1], 1);
-					close(pipex->pipe_fd[0]);
-					close(pipex->pipe_fd[1]);
-					pipex->cmd = check_cmd(pipex->avs[pipex->index + 2], pipex);
-					execve(pipex->cmd[0], pipex->cmd, pipex->envs);
-					free(pipex);
-					free_all(pipex->cmd);
-					exit(1);
-				}
+				close(pipex->pipe_fd[0]);
+				dup2(pipex->pipe_fd[1], 1);
+				close(pipex->pipe_fd[1]);
+				pipex->cmd = check_cmd(pipex->avs[pipex->index], pipex);
+				execve(pipex->cmd[0], pipex->cmd, pipex->envs);
+				free_all(pipex->cmd);
+				free(pipex);
+				exit(1);
+			}
+			else if (pipex->index == ac - 2)
+			{
+				close(pipex->pipe_fd[1]);
+				if (pipex->outfile_fd == -1)
+					ft_putstr_fd("hello\n", 2);
+				dup2(pipex->outfile_fd, 1);
+				close(pipex->pipe_fd[0]);
+				pipex->cmd = check_cmd(pipex->avs[pipex->index], pipex);
+				execve(pipex->cmd[0], pipex->cmd, pipex->envs);
+				free(pipex);
+				free_all(pipex->cmd);
+				exit(1);
 			}
 		}
-		
+		else
+		{
+			close(pipex->pipe_fd[1]);
+			dup2(pipex->pipe_fd[0], 0);
+			close(pipex->pipe_fd[0]);
+		}
 		pipex->index++;
 	}
-	while(wait(pipex->status) == -1)
-	waitpid(pipex->pid, pipex->status, WNOHANG);
+	while (wait(pipex->status) == -1)
+		waitpid(pipex->pid, pipex->status, WNOHANG);
 	free(pipex);
 	return (0);
 }
